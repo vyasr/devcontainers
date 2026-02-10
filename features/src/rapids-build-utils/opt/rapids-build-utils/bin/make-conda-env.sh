fa93970816551b9bf19be4da32d7110af6c58c7c
Vyas Ramasubramani <vyasr@nvidia.com>
2026-02-10 15:52:02 -0800

    Install opencode

--- features/src/rapids-build-utils/opt/rapids-build-utils/bin/make-conda-env.sh
+++ features/src/rapids-build-utils/opt/rapids-build-utils/bin/make-conda-env.sh
@@ -16,6 +16,16 @@
 # shellcheck disable=SC1091
 . rapids-generate-docstring;
 
+install_opencode() {
+    # Helper for opencode installation that works around curl needing
+    # authorization for the install script to query for available versions.
+    INSTALL_FILE="/tmp/install_opencode.sh"
+    curl -fsSL https://opencode.ai/install > "${INSTALL_FILE}"
+    chmod 0744 "${INSTALL_FILE}"
+    sed -i '/releases/s/curl/curl -H "Authorization: Bearer $(gh auth token)"/' "${INSTALL_FILE}"
+    ${INSTALL_FILE}
+}
+
 make_conda_env() {
     local -;
     set -euo pipefail;
@@ -56,6 +66,7 @@ make_conda_env() {
 
             mamba env create "${q[@]}" -n "${env_name}" -f "${new_env_path}";
             curl -fsSL https://claude.ai/install.sh | bash
+            install_opencode
         # If the conda env does exist but it's different from the generated one,
         # print the diff between the envs and update it
         elif ! diff -BNqw "${old_env_path}" "${new_env_path}" >/dev/null 2>&1; then
@@ -76,6 +87,7 @@ make_conda_env() {
 
             mamba env create "${q[@]}" -n "${env_name}" -f "${new_env_path}";
             curl -fsSL https://claude.ai/install.sh | bash
+            install_opencode
         fi
 
         cp -a "${new_env_path}" "${old_env_path}";
